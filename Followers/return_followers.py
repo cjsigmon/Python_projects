8171ae53431d43d3405801be565f397304e0a405
Caleb Sigmon <cjsigmon@email.unc.edu>
2022-08-06 10:52:34 -0400

    fixed issues in not referrring to correct keys

--- Followers/return_followers.py
+++ Followers/return_followers.py
@@ -16,8 +16,30 @@ with open(following_path, 'r') as j:
 # create an empty list to hold all accounts user is following that do not follow back
 no_follow_back = []
 
+
+folls = []
+# # print(no_follow_back)
+
+# i: int = 0
+
+# while i < len(followers["relationships_followers"]):
+for item in followers["relationships_followers"]:
+    folls.append(item["string_list_data"][0]["href"])
+
+for each in following["relationships_following"]:
+    if not each["string_list_data"][0]["href"] in folls:
+        no_follow_back.append(each["string_list_data"][0]["href"])
+
+
+print(no_follow_back)
+
+#     i += 1
+    
 # print(no_follow_back)
 
 
-for list_item in following["relationships_following"]:
-    print(list_item['string_list_data'][0])
+# lar = [{"a": 123, "b": 15656, "c": 77777}]
+# sba = [{"a": 123, "b": 15656, "c": 77777}]
+
+# print(lar[0] in sba)
+
